1b1525230794f36a1910bf9c264bd7e17a5ecbb2
Nineth-Nopp <nine1590kd@gmail.com>
2020-12-20 20:27:32 +0700

    rootservice,page rout and login

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.post("/signUp", function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
@@ -43,3 +42,58 @@ app.post("/signUp", function(req, res) {
         });
     });
 });
+
+app.post("/login",function(req,res){
+    const {username,password} = req.body;
+    const sql = `SELECT username, role FROM user WHERE username ='${username}'`;
+    con.query(sql, function(err,result){
+        if(err){
+            console.log(err);
+        res.status(500).send("Database server error");
+        }
+        else{
+            if(result.length != 1 ){
+                bcrypt.compare(password,result.password,function(err,total){
+                     if(total == false){
+                        res.status(400).send("Wrong password");
+                     }else{
+                        if(result[0].role == 1){
+                            res.send("admin")
+                        }else if (result[0].role == 2){
+                            res.send("user")
+                        }else{
+                            res.status(500).send("server data failed")
+                        }
+                     }
+                })
+            }
+            else{
+                res.status(500).send("Server error")
+            }
+        }
+        
+    });
+});
+
+
+
+//--root service--
+
+app.get("/", function(req, res) {
+
+    res.sendFile(path.join(__dirname,"view/index.html"))
+    
+})
+app.get("/admin", function(req, res) {
+
+    res.sendFile(path.join(__dirname,"view/admin.html"))
+    
+})
+app.get("/user", function(req, res) {
+
+    res.sendFile(path.join(__dirname,"view/user.html"))
+    
+})
+
+
+
